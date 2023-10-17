759423c2d5674f7a0de68d80f86fe41f02914e62
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-10-17 21:38:42 +0530

    Navbar added

---
+++ frontend/src/Layout/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../assets/logo.jpg';
+import './Navbar.css'; // Import your custom CSS file
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <div className="image-container">
+        <img src={logo} alt="Image1" />
+      </div>
+      <div className="list-container">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/education">Education</a></li>
+          <li><a href="/blog">Blog</a></li>
+          <li><a href="/experience">Experience</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
