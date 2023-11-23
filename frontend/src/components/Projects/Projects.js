b5cfc15e78b016538ec61fa64e56db958894efc5
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-11-23 17:48:58 +0530

    First Creation

--- frontend/src/components/Projects/Projects.js
+++ frontend/src/components/Projects/Projects.js
@@ -2,12 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../assets/Projects/leaf.png";
-import emotion from "../../assets/Projects/emotion.png";
-import editor from "../../assets/Projects/codeEditor.png";
-import chatify from "../../assets/Projects/chatify.png";
-import suicide from "../../assets/Projects/suicide.png";
-import bitsOfCode from "../../assets/Projects/blog.png";
+import portfolioImage from "../../assets/Projects/protfolio.png";
+import mailBoxImage from "../../assets/Projects/mail-box.png";
+import expenseTrackerImage from "../../assets/Projects/expense-tracker.png";
 
 function Projects() {
   return (
@@ -23,68 +20,31 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={portfolioImage}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Portfolio"
+              description="Crafting a personal portfolio website allows me to showcase my skills, projects, and experiences in a dynamic and visually compelling manner. Explore my journey, discover the projects I've undertaken, and get to know the unique blend of skills that define me. Welcome to my digital space, where innovation and creativity come to life!"
+              ghLink="https://github.com/group14000/Protfolio-React.git"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={mailBoxImage}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Mail Box"
+              description="Discover the perfect mailbox for your needs at our website! Explore a range of stylish and functional mailboxes designed to complement your home. From classic to modern, find the ideal solution that adds both charm and security. Upgrade your curb appeal and receive mail in style with our diverse collection. Browse now for a better mailbox experience!"
+              ghLink="https://github.com/group14000/Gmail-clone-mail-box.git"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={expenseTrackerImage}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Expense Tracker"
+              description="Effortlessly manage your finances with our intuitive Expense Tracker website. Gain control over your spending, track transactions, and achieve your financial goals with ease. Start your journey to financial wellness today!"
+              ghLink="https://github.com/group14000/Expense_tracker-advanvced.git"
             />
           </Col>
         </Row>
