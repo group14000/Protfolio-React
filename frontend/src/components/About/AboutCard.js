39df2c584c7ddfa76ab994c55729b35bb3f21c20
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-12-19 09:50:30 +0530

    place name is changed

--- frontend/src/components/About/AboutCard.js
+++ frontend/src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Diganta Halder </span>
-            from <span className="purple"> Kolkata, India.</span>
+            from <span className="purple"> Bangalore, India.</span>
             <br />
             I am currently looking for job as a Full Stack Developer.
             <br />
